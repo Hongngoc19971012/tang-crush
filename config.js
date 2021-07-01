38a019f23086a804a1ea60e29213d68c1baacb7c
Hongngoc19971012 <86761432+Hongngoc19971012@users.noreply.github.com>
2021-07-01 12:19:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho công chúa <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h qua đón công chúa nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100004228722741' //link mess của các bạn. VD: https://m.me/nam.nodemy
